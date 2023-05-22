f2f111ac809f8604b8bb4e4eafe6d14db9ac25d4
keith-wilde <keithw453@gmail.com>
2023-05-22 17:01:47 -0400

    Created basic component. added linting.

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+
+const welcome = {
+    greeting: "hi",
+    title: "react"
+}
+
+function getGreeting() {
+    return welcome.greeting
+}
+
+function getTitle() {
+    return welcome.title
+}
+
+const myElement
+    = React.createElement('h1', null, `${getGreeting()} ${getTitle()}`)
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            {myElement}
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text"/>
+        </div>
+    )
 }
 
 export default App
