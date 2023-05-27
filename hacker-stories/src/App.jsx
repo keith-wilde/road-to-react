e4e79f9fb254bb24f3df25e875cf50d05dd99a6a
keith-wilde <keithw453@gmail.com>
2023-05-27 16:11:54 -0400

    Removed eslint due to errors

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,61 +1,72 @@
-// import * as React from 'react';
-
+import * as React from 'react';
 
 const App = () => {
-    const stories = [{
-        title: 'React', url: 'https://reactjs.org/', author: 'Some guy', num_components: 3, points: 4, objectID: 0
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
-    }, {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Some other guy, Bill',
-        num_components: 2,
-        points: 5,
-        objectID: 1
-    }]
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
 
-    return (<div>
-        <h1>My Hacker Stores</h1>
-        <Search/>
-        <hr/>
-        <List list={stories}/>
-    </div>)
-}
+            <Search />
 
-const List = (props) => (
-    <ul>
-        {props.list.map((item) => (
-            <Item key={item.objectID} item={item}/>
-        ))}
-    </ul>
-)
+            <hr />
 
-const Item = (props) => (
-    <li>
-        <span>
-            <a href={props.item.url}>{props.item.title} </a>
-        </span>
-        <span>{props.item.author} </span>
-        <span>{props.item.num_components} </span>
-        <span>{props.item.points} </span>
-    </li>
-);
+            <List list={stories} />
+        </div>
+    );
+};
 
 const Search = () => {
     const handleChange = (event) => {
-        //synthetic event. Which is react's wrapper around browser's native event
+        // synthetic event
         console.log(event);
-        //Value of target (here: input HTML element)
+        // value of target (here: input HTML element)
         console.log(event.target.value);
     };
 
     return (
         <div>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={handleChange}/>
+            <input id="search" type="text" onChange={handleChange} />
         </div>
     );
 };
 
+const List = (props) => (
+    <ul>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item} />
+        ))}
+    </ul>
+);
 
-export default App
+const Item = (props) => (
+    <li>
+    <span>
+      <a href={props.item.url}>{props.item.title} </a>
+        {" => "}
+    </span>
+        <span>{props.item.author}</span>
+        <span>{props.item.num_comments}</span>
+        <span>{props.item.points}</span>
+    </li>
+);
+
+export default App;
