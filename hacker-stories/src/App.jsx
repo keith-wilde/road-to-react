0c81b2a7965ceb66f0040e41c6a645ccdf0a37a4
keith-wilde <keithw453@gmail.com>
2023-06-03 19:36:21 -0400

    integrated with real test endpoint.

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,25 +1,6 @@
 import * as React from 'react';
 
-
-const initialStories = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-]
-
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 const storiesReducer = (state, action) => {
     switch (action.type) {
         case 'STORIES_FETCH_INIT':
@@ -52,13 +33,6 @@ const storiesReducer = (state, action) => {
             throw new Error();
     }
 };
-const getAsyncStories = () =>
-    new Promise((resolve) =>
-        setTimeout(
-            () => resolve({data: {stories: initialStories}}),
-            2000
-        )
-    );
 
 const App = () => {
 
@@ -71,12 +45,17 @@ const App = () => {
     React.useEffect(() => {
         dispatchStories({type: "STORIES_FETCH_INIT"});
 
-        getAsyncStories()
+        fetch(`${API_ENDPOINT}fail`)
+            .then((response) => response.json())
             .then((result) => {
+                // console.log("RESULT : " + result.toJSON())
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.data.stories,
-                });
+                    payload: result.hits
+                })
+            })
+            .catch(() => {
+                dispatchStories({type: 'STORIES_FETCH_FAILURE'})
             })
             .catch(() => {
                 dispatchStories({type: "STORIES_FETCH_FAILURE"})
